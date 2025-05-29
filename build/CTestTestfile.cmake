bcd757d658ff14a22045a79036b068e41d2ddc0d
ALVARO SANTOS GARCIA <100472266@alumnos.uc3m.es>
2025-05-29 16:48:39 +0200

    First push

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alvaro/my_project
-# Build directory: /home/alvaro/my_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("share")
--- build/share/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alvaro/my_project/share
-# Build directory: /home/alvaro/my_project/build/share
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
